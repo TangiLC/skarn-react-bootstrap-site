5df395b21c3468cda33ce47c10c09ae31737a65f
TangiLC <tangi@le-cadre.net>
2023-09-25 21:23:20 +0200

    startup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
-
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
-  return (
-    <Routes>
+	return (
+		<>
+			<Header />
+			<Routes>
 				<Route path="/" element={<HomePage />} />
-        
-    </Routes>
-  );
+				<Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<Page404 />} />
+			</Routes>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
